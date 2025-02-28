663ad8fe1a41a980b420b045fc776931d3a26e5a
RichiHubb <ricardovaldes3477@gmail.com>
2025-02-27 22:00:17 -0600

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -23,22 +23,6 @@ app.config(function ($routeProvider, $locationProvider) {
         templateUrl: "/empleados",
         controller: "empleadosCtrl"
     })
-    .when("/alumnos", {
-        templateUrl: "/alumnos",
-        controller: "alumnosCtrl"
-    })
-    .when("/ventas", {
-        templateUrl: "/ventas",
-        controller: "ventasCtrl"
-    })
-    .when("/reportes", {
-        templateUrl: "/reportes",
-        controller: "reportesCtrl"
-    })
-    .when("/notificaciones", {
-        templateUrl: "/notificaciones",
-        controller: "notificacionesCtrl"
-    })
     .otherwise({
         redirectTo: "/"
     })
@@ -89,14 +73,7 @@ app.controller("appCtrl", function ($scope, $http) {
 app.controller("empleadosCtrl", function ($scope, $http) {
     // alert("Hola, soy el controlador productos")
 })
-app.controller("alumnosCtrl", function ($scope, $http) {
-})
-app.controller("ventasCtrl", function ($scope, $http) {
-})
-app.controller("reportesCtrl", function ($scope, $http) {
-})
-app.controller("notificacionesCtrl", function ($scope, $http) {
-})
+
 
 const DateTime = luxon.DateTime
 let lxFechaHora
